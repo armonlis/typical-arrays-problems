af20f57d3e0dd5e8b2ebcee1d64320da6b0ff65d
Siarhei Khonski <armonius@mail.ru>
2021-02-10 22:13:03 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  return array.length == 0 ? 0: array.sort((a, b) => a - b)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  return array.length == 0 ? 0: array.sort((a, b) => a - b)[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return array.length == 0 ? 0: array.reduce((sum, el) => sum + el, 0) / array.length;
 }
